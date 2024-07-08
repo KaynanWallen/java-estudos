4918479759528eb282ea96d585824c9799fb5326
KaynanWallen <wallenkaynan@gmail.com>
2024-07-08 16:24:37 -0300

    finish

--- screenmatch-mysql/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch-mysql/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -8,18 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
+public class ScreenmatchApplication {
 	//Gerencia o bd, posta coisas lá/atualiza
-	@Autowired
-	private SerieRepository repositorio;
-
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
 	}
 }
